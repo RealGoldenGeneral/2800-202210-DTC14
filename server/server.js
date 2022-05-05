39236411ce17aa5febc4a7f6093836c09528f503
LzhJack <zhuohan9459@gmail.com>
2022-05-05 14:51:09 -0700

    Edited server.js file with couple test page requests.

--- server/server.js
+++ server/server.js
@@ -5,3 +5,23 @@ const app = express()
 app.listen(3000, function(err){
     if(err) console.log(err);
 })
+
+app.get('/', function (req, res){
+    res.send('GET request to homepage')
+})
+
+app.get('/game', function (req, res){
+    res.send('GET request to game')
+})
+
+app.get('/quizzes', function (req, res){
+    res.send('GET request to quizzes')
+})
+
+app.get('/profile', function (req, res){
+    res.send('GET request to profile')
+})
+
+app.get('/completion', function (req, res){
+    res.send('GET request to completion')
+})
