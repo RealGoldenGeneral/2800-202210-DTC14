39bd6ba7bc543ef0aef561081a7bddeca8bdcf4d
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-06 20:37:06 -0700

    add warning if wrong info entered and redirection to homepage

--- server/public/index.js
+++ server/public/index.js
@@ -1,8 +1,15 @@
 function process_response(data) {
+    $("#incorrect-login").hide()
     console.log(data)
+    if (data != "incorrect information") {
+        location.href = "http://localhost:5005/welcome"
+    } else {
+        $("#incorrect-login").show()
+    }
 }
 
 function listenToClick() {
+    $("#incorrect-login").hide()
     $("#login").click(function() {
         $.ajax({
             type: "POST",
