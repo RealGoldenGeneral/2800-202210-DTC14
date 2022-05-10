67f16a17789173c242cb4b56bc3a034fa56db434
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-09 17:56:09 -0700

    Replaced old server code with functional code

--- server/server.js
+++ server/server.js
@@ -1,32 +1,39 @@
-const mysql = require("mysql");
 const express = require("express");
-
 const app = express();
 
-const connection = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"",
-  database:"nodejs"
-});
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
 
-connection.connect(function(error){
-  if(error) throw error
-  else console.log("connected to the database successfully!")
-})
+const cors = require('cors');
+app.use(cors())
 
-app.get("/",function(req,res){
-  res.sendFile(_dirname + "/index.html");
-})
+app.use(express.static("./public"));
 
-app.post("/",function(req,res){
-  connection.query("select * from loginuser where user_name = ? and user_pass = ?",function(error,results,fields){
-    if(results.length> 0){
-      res.redirect("/welcome");
-    } else {
-      res.redirect("/");
+function filter_password(data) {
+  return data.password
+}
+
+app.post("/login", function(req, res) {
+  console.log("post request recieved")
+  console.log(req.body.name, req.body.password)
+  username = req.body.name
+  pass = req.body.password
+  userModel.find({name: username}, function(err, user) {
+    console.log(`entered: ${pass}, in db: ${user}`)
+    var full_info = user
+    if (err) {
+      console.log(err)
+    }else {
+      user = user.map(filter_password)
+      console.log(user[0])
+      if (req.body.password == user[0]) {
+        res.send(full_info)
+      }else {
+        res.send("incorrect information")
+      }
     }
-    res.end();
   })
 })
 
@@ -34,75 +41,16 @@ app.get("/welcome",function(req,res){
   res.sendFile(__dirname + "/welcome.html")
 })
 
-async function createDB() {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    multipleStatements: true
-  })
-  const createDBandTables = await connection.query(`CREATE DATABASE IF NOT EXISTS nodejs;
-  use nodejs;
-  CREATE TABLE IF NOT EXISTS user
-  NameID int NOT NULL AUTO INCREMENT
-  name varchar(30)
-  password varchar(30)
-  PRIMARY KEY (NameID)`)
-
-  await connection.query(createDBandTables);
-
-  const [user_rows, user_fields] = await connection.query("SELECT * FROM user");
-
-  if (user_rows.length == 0) {
-    let sqlQuery = "INSERT INTO user (name, password) values ?"
-    let userRecord = [["Roy", "roypassword"]];
-    await connection.query(sqlQuery, [userRecord]);
-  }
-}
-
 app.listen(5005, function (err) {
   if (err) console.log(err);
-  else createDB();
 })
 
+const mongoose = require('mongoose');
 
-//var session = require("express-session")
-
-//const bodyparser = require("body-parser");
-// app.use(bodyparser.urlencoded({
-//   extended: true
-// }));
-
-// const cors = require('cors');
-// app.use(cors())
-
-// app.use(express.static("../public"));
-
-// app.post("/login", function (req, res) {
-//     console.log("recieved1")
-//     user_credential = {"username": req.body.name, "password": req.body.password}
-//     res.send(user_credential)
-// })
-
-// app.get('/', function (req, res) {
-//     if (req.session.authenticated) {
-//         res.send(`Hi ${req.session.user}`)
-//     } else {
-//         res.redirect('/login')
-//     }
-// })
-
-// app.get("/login", function (req, res, next) {
-//     res.send("Plesae provide the credentials through the URL")
-// })
-
-// app.get(`/login/:user/:pass`, function (req, res, next) {
-//     if (users[req.params.user] == req.params.pass) {
-//         req.session.authenticated = true
-//         req.sesssion.user = req.params.user
-//         res.send("Successful login!")
-//     } else {
-//         req.session.authenticated = false
-//         res.send("Failed login")
-//     }
-// })
+mongoose.connect("mongodb+srv://A1exander-liU:assignment3@cluster0.xi03q.mongodb.net/co-vention?retryWrites=true&w=majority",
+ {useNewUrlParser: true, useUnifiedTopology: true});
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String,
+});
+const userModel = mongoose.model("users", userSchema);
