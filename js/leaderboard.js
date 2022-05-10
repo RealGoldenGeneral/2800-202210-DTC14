6b2786f33026da1d3cd6b1927a5d8a323858daff
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-10 14:25:44 -0700

    Added swap types of leaderboards

---
+++ js/leaderboard.js
@@ -0,0 +1,42 @@
+to_add = ''
+
+function fillScoreboardQuiz() {
+    $("#leaderboard").empty()
+    to_add = ''
+    j = 10
+    for (i = 1; i <= 10; i++) {
+        to_add += `<div class="score">
+        <h4>${i}. Roy Cheng</h4>
+        <h4>${j}</h4>
+        </div>`
+        j--
+    }
+    $("#leaderboard").html(to_add)
+}
+
+function fillScoreboardGame() {
+    $("#leaderboard").empty()
+    to_add = ''
+    j = 10
+    for(i = 1; i <= 10; i++) {
+        num = 10000 * j
+        to_add += `<div class="score">
+        <h4>${i}. Roy Cheng</h4>
+        <h4>${num}</h4>
+        </div>`
+        j--
+    }
+    $("#leaderboard").html(to_add);
+}
+
+function setup() {
+    $("#gameToggle").click(() => {
+        fillScoreboardGame();
+    })
+
+    $("#quizToggle").click(() => {
+        fillScoreboardQuiz();
+    })
+}
+
+$(document).ready(setup)
