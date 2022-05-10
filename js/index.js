a7630eafb3b9f6f748b783246f23ea23a7a6a8de
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-10 15:25:58 -0700

    add click effect to news article cards

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ function process_response(data) {
     console.log(data)
 }
 
-// This code snippet was a portion from the W3Schools Horizontal Tabs How-To.
+
+// This code snippet was a portion the W3Schools Horizontal Tabs How-To
 // Adds a class (the class is defined the index.css under the navbar section) 
 // that will change the background colour to give the effect of showing the current tab.
 // You can find it here: https://www.w3schools.com/howto/howto_js_tabs.asp
@@ -11,6 +12,22 @@ function show_active_nav_item() {
     $(".navbar-item").removeClass("active")
     // Adds the class to the recently clicked navbar item
     $(this).addClass("active")
+}
+
+function return_to_normal_position() {
+    // Reset the y-position and box-shadow values back to their default
+    $(".news-card").css({"transform": "translate(0, 0)", "box-shadow": "0px 4px 4px rgb(0, 0, 0, 0.25)"})
+}
+
+function show_click_effect() {
+    // Adapted from a W3Schools How-To on how to simulate button pressed effect.
+    // Since the click effect is happening on a div element and not a button, the action was
+    // done through JS instead.
+    // You can find it here: https://www.w3schools.com/howto/howto_css_animate_buttons.asp
+    // Once a news article card is clicked, move the card down 2px and reduce the box-shadow effect
+    $(this).css({"transform": "translate(0, 2px)", "box-shadow": "0px 2px 0px rgb(0, 0, 0, 0.25)"})
+    // Call the callback, 'return_to_normal_position' after 150ms
+    setTimeout(return_to_normal_position, 150)
 }
 
 function listenToClick() {
