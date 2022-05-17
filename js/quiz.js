2593e759d49b0b6f1790d26520d994c12f605740
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-16 20:55:09 -0700

    add sliding animation when clicking prev or next button

--- js/quiz.js
+++ js/quiz.js
@@ -2,6 +2,8 @@ current_slide = 0
 
 function show_prev_quiz_info() {
     $(".quiz_score_info").hide()
+    $(".quiz_score_info").removeClass("slide_right")
+    $(".quiz_score_info").addClass("slide_left")
     slides = $(".quiz_info_slideshow_container").children(".quiz_score_info")
     if (current_slide <= 0) {
         current_slide = slides.length - 1
@@ -19,6 +21,8 @@ function show_next_quiz_info() {
     console.log(slides[0])
     console.log(slides[1])
     $(".quiz_score_info").hide()
+    $(".quiz_score_info").removeClass("slide_left")
+    $(".quiz_score_info").addClass("slide_right")
     if (current_slide >= (slides.length - 1)) {
         current_slide = 0
         console.log("at highest slide")
