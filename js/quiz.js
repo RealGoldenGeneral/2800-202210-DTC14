74e1e95f73700741410238406b5a1c3b774ce39e
AlexanderLiu <alexanderliu2016@gmail.com>
2022-05-18 12:22:40 -0700

    add request and route to get quiz questions

--- js/quiz.js
+++ js/quiz.js
@@ -1,15 +1,27 @@
+////
+// Referenced the slideshow how-to from W3Schools, can be found here: https://www.w3schools.com/howto/howto_js_slideshow.asp
+// All code was changed to use jQuery events and handlers instead, no inline js, event handling is done in js file
+// Code was divided into two functions, one for clicking previous and one for clicking next
+
+// defaulting current slide to 0, which is start of array
 current_slide = 0
 
 function show_prev_quiz_info() {
+    // hide all intitally
     $(".quiz_score_info").hide()
+    // removeClass and addClass for css animations
     $(".quiz_score_info").removeClass("slide_right")
     $(".quiz_score_info").addClass("slide_left")
+    // gets array of child elemenents of class quiz_info_slideshow_container who have class, quiz_score_info
     slides = $(".quiz_info_slideshow_container").children(".quiz_score_info")
+    // to make sure you don't go to index, -1 and below
     if (current_slide <= 0) {
         current_slide = slides.length - 1
+        // display HTML of the child at index X of the array
         $(slides[current_slide]).show()
     }
     else {
+        // subtract indexing value since it is previous 
         current_slide -= 1
         $(slides[current_slide]).show()
     }
@@ -23,18 +35,23 @@ function show_next_quiz_info() {
     $(".quiz_score_info").hide()
     $(".quiz_score_info").removeClass("slide_left")
     $(".quiz_score_info").addClass("slide_right")
+    // to make sure you don't go above max index of the array
     if (current_slide >= (slides.length - 1)) {
+        // return to first slide if press next at last slide
         current_slide = 0
         console.log("at highest slide")
         $(slides[current_slide]).show()
     }
     else {
+        // add 1 to indexing value since it is next
         current_slide += 1
         $(slides[current_slide]).show()
     }
 }
+////
 
 function setup() {
+    // hiding and showing defaulted quiz info section
     $(".quiz_score_info").hide()
     $("#covid_safety").show()
     $(".next").click(show_next_quiz_info)
