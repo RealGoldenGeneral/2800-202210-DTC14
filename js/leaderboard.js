25731bd895aabd8171227d540b388fcd47cf7c26
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-20 13:53:03 -0700

    debugged displaying of quiz

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -5,11 +5,12 @@ function fillScoreboardQuiz() {
         type: "get",
         url: "/getQuizRecords",
         success: (data) => {
+            console.log(data)
             $("#leaderboard").empty()
             to_add = ''
             for (i = 0; i < data.length; i++) {
                 to_add += `<div class="score">
-                <h4>${i + 1}. ${data[i].name}/h4>
+                <h4>${data[i].category}</h4>
                 <h4>${data[i].high_score}</h4>
                 </div>`
             }
